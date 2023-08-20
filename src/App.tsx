46d47dba1723274095b9eebb01b7e28095e739ff
Alexander Korobov <dktwho@mail.ru>
2023-08-20 17:37:25 +0300

    modified Associative array, fixed changeFilterValue, modified callback add one more args

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ export type TodoListsType = {
 }
 
 function App() {
-
-
     let todolistID1 = v1()
     let todolistID2 = v1()
 
@@ -21,29 +19,18 @@ function App() {
         {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    // let [tasks, setTasks] = useState({
-    //     [todolistID1]: [
-    //         {id: v1(), title: 'HTML&CSS', isDone: true},
-    //         {id: v1(), title: 'JS', isDone: true},
-    //         {id: v1(), title: 'ReactJS', isDone: false},
-    //
-    //     ],
-    //     [todolistID2]: [
-    //         {id: v1(), title: 'Rest API', isDone: true},
-    //         {id: v1(), title: 'GraphQL', isDone: false},
-    //     ]
-    // })
+    let [tasks, setTasks] = useState({
+        [todolistID1]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
-
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
-
+        ],
+        [todolistID2]: [
+            {id: v1(), title: 'Rest API', isDone: true},
+            {id: v1(), title: 'GraphQL', isDone: false},
+        ]
+    })
 
     function removeTask(id: string) {
         // let filteredTasks = tasks.filter(t => t.id !== id);
@@ -67,7 +54,6 @@ function App() {
 
 
     function changeFilter(todoListId: string, value: FilterValuesType) {
-        // setFilter(value);
         setTodolists(todolists.map(tl => tl.id === todoListId ? {...tl, filter: value } : tl))
     }
 
@@ -75,12 +61,12 @@ function App() {
     return (
         <div className="App">
             {todolists.map(tl => {
-                let tasksForTodolist = tasks;
+                let tasksForTodolist = tasks[tl.id];
                 if (tl.filter === "active") {
-                    tasksForTodolist = tasks.filter(t => t.isDone === false);
+                    tasksForTodolist = tasks[tl.id].filter(t => t.isDone === false);
                 }
                 if (tl.filter === "completed") {
-                    tasksForTodolist = tasks.filter(t => t.isDone === true);
+                    tasksForTodolist = tasks[tl.id].filter(t => t.isDone === true);
                 }
 
                 return (
