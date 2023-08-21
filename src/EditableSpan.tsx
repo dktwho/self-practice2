a1953e6e7b8a3f0e608729c612041ae3068b92f5
Alexander Korobov <dktwho@mail.ru>
2023-08-21 17:34:22 +0300

    add component to EditableSpan editHandler, onBlur, autofocus, onDoubleClick for span, and onBlur for input

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type SpanType = {
     oldTitle: string
 }
 export const EditableSpan = (props: SpanType) => {
+    const [edit, setEdit] = useState(false)
+    const editHandler = () => {
+        setEdit(!edit)
+    }
+
     return (
-        <span>{props.oldTitle}</span>
+        edit ? <input type="text" onBlur={editHandler} autoFocus/>
+            : <span onDoubleClick={editHandler}>{props.oldTitle}</span>
+
     );
 };
 
