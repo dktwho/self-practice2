11b9055fe2957d796843d9be19b24b7139fac82f
Alexander Korobov <dktwho@mail.ru>
2023-08-21 17:41:02 +0300

    add local state for onChange

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type SpanType = {
     oldTitle: string
 }
 export const EditableSpan = (props: SpanType) => {
     const [edit, setEdit] = useState(false)
+    const [newTitle, setNewTitle] = useState(props.oldTitle)
     const editHandler = () => {
         setEdit(!edit)
     }
 
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(e.currentTarget.value)
+    }
     return (
-        edit ? <input type="text" onBlur={editHandler} autoFocus/>
+        edit ? <input value={newTitle} onChange={onChangeHandler} type="text" onBlur={editHandler} autoFocus/>
             : <span onDoubleClick={editHandler}>{props.oldTitle}</span>
 
     );
