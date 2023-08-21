4cc0a8505684ab1306ee59fc337ff4acf9b18764
Alexander Korobov <dktwho@mail.ru>
2023-08-21 11:00:53 +0300

    change data, nested level 2

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,43 @@ function App() {
     let todolistID2 = v1()
 
     let [todoLists, setTodoLists] = useState<Array<TodoListsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
+        {id: todolistID1, title: 'What to learn'},
+        {id: todolistID2, title: 'What to buy' },
     ])
 
-    let [tasks, setTasks] = useState({
-        [todolistID1]: [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true},
-            {id: v1(), title: 'ReactJS', isDone: false},
+    // let [tasks, setTasks] = useState({
+    //     [todolistID1]: [
+    //         {id: v1(), title: 'HTML&CSS', isDone: true},
+    //         {id: v1(), title: 'JS', isDone: true},
+    //         {id: v1(), title: 'ReactJS', isDone: false},
+    //     ],
+    //     [todolistID2]: [
+    //         {id: v1(), title: 'Rest API', isDone: true},
+    //         {id: v1(), title: 'GraphQL', isDone: false},
+    //     ]
+    // })
 
-        ],
-        [todolistID2]: [
-            {id: v1(), title: 'Rest API', isDone: true},
-            {id: v1(), title: 'GraphQL', isDone: false},
-        ]
+
+    let [tasks, setTasks] = useState({
+        [todolistID1]:  {
+            data: [
+                {id: v1(), title: 'HTML&CSS', isDone: true},
+                {id: v1(), title: 'JS', isDone: true},
+                {id: v1(), title: 'ReactJS', isDone: false},
+            ],
+            filter: 'all'
+        }
+
+        ,
+        [todolistID2]:  {
+            data: [
+                {id: v1(), title: 'Rest API', isDone: true},
+                {id: v1(), title: 'GraphQL', isDone: false},
+            ],
+            filter: 'all'
+        }
+
+
     })
 
     function removeTask(todoListId: string, id: string) {
